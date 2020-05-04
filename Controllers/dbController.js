f8790c3223545326a103aca0e98cedc1175b6d92
Mikkel Jepsen <Herom52@msn.com>
2020-05-04 09:07:55 +0200

    Updated dbcontroller with getusername and get userpassword methods, WIP gui done

--- Controllers/dbController.js
+++ Controllers/dbController.js
@@ -12,19 +12,23 @@ const db = admin.firestore();
 exports.getUserNames = async () => {
     const snapshot = await db.collection("Users").get()
     if (snapshot.empty) {
-        console.log("her2");
         return;
     } else {
         let result = [];
         snapshot.forEach(element => {
             result.push(element.data().Name);
-            console.log(element.data().Name);
-
         });
-        console.log(result);
-
+        result.sort()
         return result;
-
-
+    }
+}
+exports.getSpecificUserPassword = async (userName) => {
+    const snapshot = await db.collection("Users")
+        .where("Username", "==", userName).get();
+    if (snapshot.empty) {
+        return;
+    } else {
+        const hashedPassword = snapshot.docs[0].data().Password
+        return hashedPassword;
     }
 }
--- Tests/userListUnitTest.spec.js
+++ Tests/userListUnitTest.spec.js
@@ -1,16 +1,27 @@
 const assert = require("chai").assert
-const chai = require("chai")
-const chaiAsPromised = require("chai-as-promised")
-chai.use(chaiAsPromised)
-const expect = require("chai").expect
-const { getUserNames } = require("../Controllers/getUsers")
+const { getUserNames, getSpecificUserPassword } = require("../Controllers/dbController")
 
+let result = []
+before(async () => {
+    result = await getUserNames();
+})
 describe("Test of getting users from DB", () => {
-    it("Should return P1", async () => {
-        let result = await getUserNames();
-        console.log("result: " + result);
-
-        assert.equal(result, "Person1");
+    it("Person1 exists in the database", () => {
+        assert.equal(result.includes("Person1"), true)
     })
-
+    it("Thor does not exist in the database", () => {
+        assert.equal(result.includes("Thor"), false)
+    })
+    it("Returns all names in DB", () => {
+        assert.equal(result.includes("Anna", 0), true)
+        assert.equal(result.includes("Person1", 1), true)
+        assert.equal(result.includes("Tester2", 2), true)
+        assert.equal(result.length, 3)
+    })
+})
+describe("Test get user password", () => {
+    it("Returns correct password", async () => {
+        const password = await getSpecificUserPassword("T2")
+        assert.equal(password, "0987")
+    })
 })
