a79cac0aedeaa54ad70b99d360acefe116c64a31
Thor Henriksen <thor.henriksen.97@gmail.com>
2020-05-14 10:30:29 +0200

    Updated documentation

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -3,28 +3,28 @@ const loginController = require('./LoginController');
 
 /**
  * Function that creates a new user in the database collection 'Users' by making call to the createUser function in DBController.js
- * @param {*} admin a value representing whether or not user is administrator. Should be either '0' or '1'
- * @param {*} name of user
- * @param {*} email of user
- * @param {*} username of user
- * @param {*} password (plaintext) to be salted and hashed by the saltAndHashPassword function of LoginController.js
- * @param {*} permissions array of a given users privileges. Values should be the name of the training forms as a string the user has acces to.
- * @param {*} title job title of user. Should be either 'Leder', 'Instruktoer', or 'Rengoering'
+ * @param {String} admin a value representing whether or not user is administrator. Should be either '0' or '1'
+ * @param {String} name of user
+ * @param {String} email of user
+ * @param {String} username of user
+ * @param {String} password (plaintext) to be salted and hashed by the saltAndHashPassword function of LoginController.js
+ * @param {Array} permissions array of a given users privileges. Values should be the name of the training forms as a string the user has acces to.
+ * @param {Array} titles job title of user. Should be either 'Leder', 'Instruktoer', or 'Rengoering'
  * 
  * returns the newly created user object
  */
-async function createNewUser(admin, name, email, username, password, permissions, title) {
+async function createNewUser(admin, name, email, username, password, permissions, titles) {
 
     let hashedPassword = await loginController.saltAndHashPassword(password);
 
-    let newUser = dbController.createUser(admin, name, email, username, hashedPassword, permissions, title);
+    let newUser = dbController.createUser(admin, name, email, username, hashedPassword, permissions, titles);
 
     return newUser;
 }
 
 /**
  * Function that makes a select from the database by making call to the getUser function of DBController.js.
- * @param {*} username of the user one wants to find in the DB.
+ * @param {String} username of the user one wants to find in the DB.
  * 
  * Returns not the user object, but select variables from the user object. For use in acces control.
  */
